ecde88704fe38eac505d01c01915e49d300d48eb
Ravi Kant <75370682+topdogfound@users.noreply.github.com>
2023-04-12 23:01:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+$server = "";
+$username = "";
+$password = "";
+$dbname = "";
+$conn = mysql_connect($server, $username, $password, $dbname);
+if(!$conn){
+  die("Connection Failed".mysqli_connect_error()); 
+}
 include "header1.php";
 include "body.php";
 include "footer.php";
