92c3e4a202bf27abcb377210af83133c1e728e8e
Ravi Kant <75370682+topdogfound@users.noreply.github.com>
2023-04-12 23:03:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$server = "";
-$username = "";
-$password = "";
-$dbname = "";
+$server = "sql307.epizy.com";
+$username = "epiz_33997484";
+$password = " X7bx1yacBgMQxV ";
+$dbname = "epiz_33997484_topdogfound";
 $conn = mysql_connect($server, $username, $password, $dbname);
 if(!$conn){
   die("Connection Failed".mysqli_connect_error()); 
